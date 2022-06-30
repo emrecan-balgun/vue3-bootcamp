7f41f2218cf444a66d69546bd53b50db6cb33120
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-30 14:35:35 +0300

    update reactivity

--- Week-1/4-reactivity-examples/app.js
+++ Week-1/4-reactivity-examples/app.js
@@ -1,7 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            itemList: ["elma", "kivi", "çilek", "kavun"]
+            search: null,
+            itemList: ["elma", "kivi", "çilek", "kavun"],
+            // filteredList: []
         }
+    },
+    methods: {
+        searchList() {
+            // this.filteredList = this.itemList.filter(item => item.includes(this.search))
+        },
+    },
+    computed: {
+        filteredList() {
+            return this.itemList.filter(item => item.includes(this.search))
+        },
     },
 }).mount("#app");

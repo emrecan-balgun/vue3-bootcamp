ac67e0b1e25bf6464b91e9c847392d9219f9ecfc
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-04 10:35:49 +0300

    update 0-component and add 1-vue-cli

--- Week-2/0-component/app.js
+++ Week-2/0-component/app.js
@@ -1,7 +1,22 @@
 const app = Vue.createApp({
     data() {
+        return {};
+    },
+})
+
+app.component("counter-item", {
+    data() {
         return {
             counter: 0,
         };
     },
-}).mount("#app");
+    template: `
+        <div class="container-sm text-center">
+            <h3 class="mb-2">{{ counter }}</h3>
+            <button @click="counter++" class="red md">+</button>
+            <button @click="counter--" class="green md">-</button>
+        </div>
+    `
+})
+
+app.mount("#app");

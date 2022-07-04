8f7a1689332d9881e4f4f562878cae9115d885ac
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-04 11:59:18 +0300

    add 2-todo-app

---
+++ Week-2/2-todo-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h3 class="text-center">ToDo App</h3>
+    <hr class="my-2">
+    <label for="todoText"></label>
+    <input type="text" id="todoText" placeholder="Bir şeyler yazınız...">
+    <ul>
+      <li 
+        v-for="i in 5" 
+        :key="i" 
+        class="d-flex justify-content-between align-items-center">
+        <span>Todo Item #{{ i }}</span>
+        <button class="sm red">Sil</button>
+      </li>
+    </ul>
+    <small class="d-flex justify-content-end green mt-2">5 adet todo var.</small>
+  </div>
+</template>

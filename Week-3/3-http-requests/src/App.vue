16f83ab0db89fb44a10a36955ed9c615b73bd79f
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-05 14:10:45 +0300

    add axios, json server and update 3-http-requests

--- Week-3/3-http-requests/src/App.vue
+++ Week-3/3-http-requests/src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h3 class="mb-1">Alışveriş Listesi</h3>
+    <hr />
+    <div class="my-2">
+      <input type="text" placeholder="Ne alacaksın?" @keydown.enter="onSave" />
+    </div>
+    <ul v-if="itemsList.length > 0">
+      <li v-for="item in itemsList" :key="item.id" class="d-flex justify-content-between align-items-center">
+        <span>{{ item.title }}</span>
+        <button @click="onDelete(item)" class="sm red">Sil</button>
+      </li>
+    </ul>
+    <div v-else class="bg-blue text-white">
+      Herhangi bir ürün yok.
+    </div>
+    <small class="text-red d-flex justify-content-end align-items-center mt-2">Alınacak {{ itemCount }} adet ürün vardır..</small>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      itemsList: []
+    }
+  },
+  mounted() {
+    axios.get("http://localhost:3000/items").then(items_response => {
+      console.log('items_response', items_response);
+      this.itemsList = items_response.data || [];
+    })
+  },
+  methods: {
+    onSave(event) {
+      const saveObject = {
+        title: event.target.value,
+        created_at: new Date(),
+        completed: false
+      };
+
+      axios.post("http://localhost:3000/items", saveObject).then(save_response => {
+        console.log(save_response);
+        this.itemsList.push(save_response.data);
+        event.target.value = "",
+        event.target.focus();
+      })
+    },
+    onDelete(item) {
+      axios.delete(`http://localhost:3000/items/${item.id}`).then(delete_response => {
+        console.log('delete_response', delete_response)
+        this.itemsList = this.itemsList.filter(i => i.id !== item.id)
+      })
+    }
+  },
+  computed: {
+    itemCount() {
+      return this.itemsList.length || 0;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

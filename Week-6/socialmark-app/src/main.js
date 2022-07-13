19acd5fb73273c038b9c36ac974a0d152f2406dd
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-13 15:05:04 +0300

    add appHeader, sidebar and update main.js

--- Week-6/socialmark-app/src/main.js
+++ Week-6/socialmark-app/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import '@/assets/style.css';
+import appHeader from '@/components/Shared/appHeader';
 
 const app = createApp(App);
+app.component("appHeader", appHeader);
 app.use(router);
 app.mount('#app');

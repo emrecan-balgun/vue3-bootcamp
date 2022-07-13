527a5bc1a244a5860a5f1e2f780795a0262d663a
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-13 14:48:08 +0300

    add views and update router

--- Week-6/socialmark-app/src/router/index.js
+++ Week-6/socialmark-app/src/router/index.js
@@ -4,7 +4,18 @@ const routes = [
     {
         name: "HomePage",
         path: "/",
-    }
+        import: () => import('@/views/Home'),
+    },
+    {
+        name: "LoginPage",
+        path: "/login",
+        import: () => import('@/views/Login'),
+    },
+    {
+        name: "RegisterPage",
+        path: "/register",
+        import: () => import('@/views/Register'),
+    },
 ]
 
 export default createRouter({

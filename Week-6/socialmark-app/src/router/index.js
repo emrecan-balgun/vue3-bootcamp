0968c5d334531ed98c0b94e140be940391e11403
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-14 15:14:57 +0300

    update style.css and router

--- Week-6/socialmark-app/src/router/index.js
+++ Week-6/socialmark-app/src/router/index.js
@@ -1,49 +1,47 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import store from '../store';
+import { createRouter, createWebHashHistory } from "vue-router";
+import store from "../store";
 
 const routes = [
-    {
-        name: "HomePage",
-        path: "/",
-        component: () => import('@/views/Home'),
-    },
-    {
-        name: "LoginPage",
-        path: "/login",
-        component: () => import('@/views/Login'),
-    },
-    {
-        name: "RegisterPage",
-        path: "/register",
-        component: () => import('@/views/Register'),
-    },
-    {
-        name: "NewBookmarkPage",
-        path: "/new",
-        component: () => import('@/views/NewBookmark'),
-    },
-]
+  {
+    name: "HomePage",
+    path: "/",
+    component: () => import("@/views/Home")
+  },
+  {
+    name: "LoginPage",
+    path: "/login",
+    component: () => import("@/views/Login")
+  },
+  {
+    name: "RegisterPage",
+    path: "/register",
+    component: () => import("@/views/Register")
+  },
+  {
+    name: "NewBookmarkPage",
+    path: "/new",
+    component: () => import("@/views/NewBookmark")
+  },
+];
 
 const router = createRouter({
-    routes,
-    history: createWebHashHistory() 
-})
+  routes,
+  history: createWebHashHistory()
+});
 
-router.beforeEach((to, from, next) => {
-    const authRequiredRoutes = ["HomePage"];
-    const authNotRequiredRoutes = ["LoginPage", "RegisterPage"];
+router.beforeEach((to, _, next) => {
+  const authRequiredRoutes = ["HomePage"];
+  const authNotRequiredRoutes = ["LoginPage", "RegisterPage"];
+  const _isAuthenticated = store.getters._isAuthenticated;
 
-    if(authNotRequiredRoutes.indexOf(to.name) > -1 && _isAuthenticated) {
-        next(false);
-    }
+  if (authNotRequiredRoutes.indexOf(to.name) > -1 && _isAuthenticated) next(false);
 
-    const _isAuthenticated = store.getters._isAuthenticated;
-    if(authRequiredRoutes.indexOf(to.name) > -1) {
-        if(_isAuthenticated) next();
-        else next({ name : "LoginPage" });
-    } else {
-        next();
-    }
-})
+  if (authRequiredRoutes.indexOf(to.name) > -1) {
+    if (_isAuthenticated) next();
+    else next({ name: "LoginPage" });
+  } else {
+    next();
+  }
+});
 
 export default router;

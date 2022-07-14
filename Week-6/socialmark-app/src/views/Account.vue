9f357f104b160889fce224355bafafdae03661d3
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-14 18:14:51 +0300

    add account, favorites and sideBar and update appHeader, router

---
+++ Week-6/socialmark-app/src/views/Account.vue
@@ -0,0 +1,28 @@
+<template>
+    <app-header />
+    <div class="flex flex-row">
+        <side-bar />
+        <component :is="$route.meta.componentName" :items="bookmarkList" />
+    </div>
+</template>
+
+<script>
+import sideBar from '@/components/Account/sideBar';
+
+export default {
+    components: {
+        sideBar,
+    },
+    data() {
+        return {
+            bookmarkList: []
+        }
+    },
+    created() {
+        this.$appAxios.get("/bookmarks?_expand=category&_expand=user").then(bookmark_list_response => {
+            console.log(bookmark_list_response);
+            this.bookmarkList = bookmark_list_response?.data || [];
+        })
+    },
+}
+</script>

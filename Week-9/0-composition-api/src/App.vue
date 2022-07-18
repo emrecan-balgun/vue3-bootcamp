94285fad33039f545d08277e58c6220736d81fa9
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-18 15:53:10 +0300

    add counter

--- Week-9/0-composition-api/src/App.vue
+++ Week-9/0-composition-api/src/App.vue
@@ -4,10 +4,19 @@
   {{ titleLengthMessage }}
   <button @click="toggleIt">Toggle</button>
   <p v-if="show">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis porro quisquam commodi consequuntur corrupti fugit beatae consectetur consequatur, laborum aliquam.</p>
+  <hr />
+  <div>
+    <h3>{{ counter }} : {{ oddOrEven }}</h3>
+    <div>
+        <button @click="counter++">Arttır</button>
+        <button @click="counter--">Azalt</button>
+        <button @click="counter = 0">Sıfırla</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, computed } from "vue"; // ref: reactive effect
+import { ref, computed, watch } from "vue"; // ref: reactive effect
 
 export default {
   // data() {
@@ -17,26 +26,34 @@ export default {
   // },
   setup() {
     const title = ref("This is test title");
+    // const titleLengthMessage = computed(() => {
+    //   return title.value.length + " adet karakter yazdınız.";
+    // })
+    const titleLengthMessage = computed(() => title.value.length + " adet karakter yazdınız.");
+    // console.log("titleLengthMessage => ", titleLengthMessage.value);
+
     const show = ref(false);
     console.log("show => ", show.value);
-
     // const toggleIt = () => {}
     function toggleIt() {
       show.value = !show.value;
     }
 
-    // const titleLengthMessage = computed(() => {
-    //   return title.value.length + " adet karakter yazdınız.";
-    // })
-    const titleLengthMessage = computed(() => title.value.length + " adet karakter yazdınız.");
-
-    // console.log("titleLengthMessage => ", titleLengthMessage.value);
+    const counter = ref(0);
+    const oddOrEven = computed(() => counter.value % 2 == 0 ? '(Çift sayı)' : '(Tek sayı)');
+    // watch(counter, () => {}, { deep: true })
+    // watch([counter, oddOrEven], (newValue, oldValue) => {
+    watch([counter, oddOrEven], ([newC, newO], [oldC, oldO]) => {
+      console.log(oldO, " -> ", newO)
+    })
 
     return {
       title,
       show,
       toggleIt,
-      titleLengthMessage
+      titleLengthMessage,
+      counter,
+      oddOrEven
     }
   }
 }

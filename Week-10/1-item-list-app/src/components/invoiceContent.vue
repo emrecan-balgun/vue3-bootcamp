d8034e587588220bd5cc43679a66210ee913481e
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-19 13:35:47 +0300

    refactor  components

--- Week-10/1-item-list-app/src/components/invoiceContent.vue
+++ Week-10/1-item-list-app/src/components/invoiceContent.vue
@@ -1,12 +1,12 @@
 <template>
  <section class="bg-gray-900 w-1/3 mx-auto mt-10 p-2 px-5 rounded-md shadow-2xl">
       <!-- Fatura Bilgileri -->
-      <contact-section />
+      <contact-section :contact="state.contact" />
       <div class="mt-5">
         <heading title="Fatura Kalemleri" />
-        <invoice-items />
+        <invoice-items :items="state.items" :AddInvoiceItem="AddInvoiceItem" />
       </div>
-      
+
       <!-- Summary -->
       <invoice-summary />
       <hr class="bg-gradient-to-r h-[1px] border-none from-gray-700 mt-5" />
@@ -20,4 +20,32 @@
 import invoiceItems from './invoiceItems.vue';
 import invoiceSummary from './invoiceSummary.vue';
 import contactSection from './contactSection.vue';
+import { reactive, provide } from 'vue';
+
+const state = reactive({
+  contact: {
+    contact_name: null,
+    email: null,
+    city: null,
+    country: null,
+    zipcode: null,
+  },
+  items: [],
+});
+
+const AddInvoiceItem = () => {
+  state.items.push({
+    id: new Date().getTime(),
+    name: null,
+    quantity: 0,
+    unit_price: 0.0,
+    total_price: 0.0,
+  })
+};
+
+const DeleteInvoiceItem = (invoiceItem) => {
+  state.items = state.items.filter(i => i.id !== invoiceItem.id)
+};
+
+provide("DeleteInvoiceItem", DeleteInvoiceItem);
 </script>
--- Week-10/1-item-list-app/src/components/invoiceItems.vue
+++ Week-10/1-item-list-app/src/components/invoiceItems.vue
@@ -6,8 +6,8 @@
         <span class="p-1 w-[100px]">Toplam</span>
     </div>
     <div class="table-body">
-        <invoice-item v-for="i in 5" :key="i" />
-        <button class="mt-4 add-item-button">
+        <invoice-item v-for="item in items" :key="item.id" :item="item" />
+        <button @click="AddInvoiceItem" class="mt-4 add-item-button">
         <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
             <path d="M0 0h24v24H0V0z" fill="none" />
             <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
@@ -19,4 +19,6 @@
 
 <script setup>
 import invoiceItem from './invoiceItem.vue';
+
+defineProps({ items: Array, AddInvoiceItem: Function });
 </script>

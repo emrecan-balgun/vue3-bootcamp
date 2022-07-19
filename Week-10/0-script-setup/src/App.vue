77196a25c69b78ff47cc95fbf1fbce293d7ae8bb
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-19 10:20:42 +0300

    create vue project (with vite)

---
+++ Week-10/0-script-setup/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>{{ title }}</h3>
+  <input type="text" v-model="title" >
+  <button @click="increase">{{ counter }}</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+  const title = ref("Test");
+  const counter = ref(0);
+
+  const increase = () => {
+    counter.value++; // ref ile tutulduğu için value üzerinden ulaşılıyor
+  }
+
+</script>

dcf99a5cedd8ebd3fbfeeb973f2a1f26df4c3882
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-19 10:34:10 +0300

    add oddOrEven and update app.vue

--- Week-10/0-script-setup/src/App.vue
+++ Week-10/0-script-setup/src/App.vue
@@ -2,16 +2,23 @@
   <h3>{{ title }}</h3>
   <input type="text" v-model="title" >
   <button @click="increase">{{ counter }}</button>
+  <hr />
+  <oddOrEven :counter="counter" @odd-event="alertMe" />
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import oddOrEven from './components/oddOrEven.vue';
 
   const title = ref("Test");
   const counter = ref(0);
 
   const increase = () => {
     counter.value++; // ref ile tutulduğu için value üzerinden ulaşılıyor
+  }
+
+  const alertMe = (info) => {
+    console.log(info);
   }
 
 </script>
---
+++ Week-10/0-script-setup/src/components/oddOrEven.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ props.counter }} {{ result }}</h1>
+</template>
+
+<script setup>
+import { computed, watch } from 'vue';
+
+const props = defineProps({ counter: Number });
+const emit = defineEmits(["odd-event"]); //dokümante etmek için yazdık, aşağıda ki emitin çalışmasıyla ilgisi yok
+const result = computed(() => props.counter % 2 == 0 ? '(Çift)' : '(Tek)');
+
+watch(result, (result) => {
+    if(result === '(Tek)')
+        emit("odd-event", true);
+})
+
+</script>

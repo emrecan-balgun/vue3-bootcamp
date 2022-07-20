362eaab0031ffcbbf137f475c737c4bf2d84daff
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-20 18:33:43 +0300

    add forms components

---
+++ Week-11/2-namespaced-components/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// import Input from './components/Forms/Input.vue';
+// import Label from './components/Forms/Label.vue';
+// import Select from './components/Forms/Select.vue';
+import * as Form from './components/form-components';
+</script>
+
+<template>
+  <!-- <Label>
+    Başlık
+    <Input />
+  </Label>
+  <Label>
+    Kategori
+    <Select />
+  </Label> -->
+  <Form.Label>
+    Başlık
+    <Form.Input />
+  </Form.Label>
+  <Form.Label>
+    Kategori
+    <Form.Select />
+  </Form.Label>
+</template>

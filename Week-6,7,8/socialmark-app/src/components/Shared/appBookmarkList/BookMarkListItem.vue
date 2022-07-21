85c8e82007d9ef6d107a6c921303b716120e2b74
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-21 11:57:08 +0300

    add BookmarkListItem.js (composables) and refactor BookMarkListItem.vue

--- Week-6,7,8/socialmark-app/src/components/Shared/appBookmarkList/BookMarkListItem.vue
+++ Week-6,7,8/socialmark-app/src/components/Shared/appBookmarkList/BookMarkListItem.vue
@@ -1,3 +1,17 @@
+<script setup>
+/* eslint-disable no-unused-vars */
+import { defineProps } from "vue";
+import BookmarkListItem from "@/composables/BookmarkListItem";
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true,
+    default: () => {}
+  }
+});
+const { likeItem, bookmarkItem, categoryName, userName, alreadyBookmarked, alreadyLiked, bookmarkedItem, likedItem, _getCurrentUser, _userBookmarks, _userLikes } = BookmarkListItem(props.item);
+</script>
+
 <template>
   <div class="bg-white flex flex-col gap-x-3 rounded-md shadow-sm">
     <div class="p-3">
@@ -49,67 +63,3 @@
     <div class="bg-red-200 p-1 mt-auto text-red-900 text-center text-sm">{{ categoryName }}</div>
   </div>
 </template>
-
-<script setup>
-import { defineProps, inject, computed } from 'vue';
-import { useStore } from 'vuex';
-
-const store = useStore();
-const appAxios = inject('appAxios');
-const props = defineProps({
-  item: {
-      type: Object,
-      required: true,
-      default: () => {}
-    }
-});
-
-    const likeItem = () => {
-      appAxios({
-        url: alreadyLiked.value ? `/user_likes/${likedItem.value.id}` : "/user_likes",
-        method: alreadyLiked.value ? "DELETE" : "POST",
-        data: {
-          userId: _getCurrentUser.value.id,
-          bookmarkId: props.item.id
-        }
-      }).then(user_like_response => {
-        let bookmarks = [..._userLikes.value];
-        if (alreadyLiked.value) {
-          bookmarks = bookmarks.filter(b => b.id !== likedItem.value.id);
-        } else {
-          bookmarks = [...bookmarks, user_like_response.data];
-        }
-        store.commit("setLikes", bookmarks);
-      });
-    };
-
-    const bookmarkItem = () => {
-      appAxios({
-        url: alreadyBookmarked.value ? `/user_bookmarks/${bookmarkedItem.value.id}` : "/user_bookmarks",
-        method: alreadyBookmarked.value ? "DELETE" : "POST",
-        data: {
-          userId: _getCurrentUser.value.id,
-          bookmarkId: props.item.id
-        }
-      }).then(user_bookmark_response => {
-        let bookmarks = [..._userBookmarks.value];
-        if (alreadyBookmarked.value) {
-          bookmarks = bookmarks.filter(b => b.id !== bookmarkedItem.value.id);
-        } else {
-          bookmarks = [...bookmarks, user_bookmark_response.data];
-        }
-        store.commit("setBookmarks", bookmarks);
-      });
-    };
-
-    const categoryName = computed(() => props.item?.category?.name || '-');
-    const userName = computed(() => props.item?.user?.fullname || '-');
-    const alreadyLiked = computed(() => Boolean(likedItem.value));
-    const alreadyBookmarked = computed(() => Boolean(bookmarkedItem.value));
-    const bookmarkedItem = computed(() => _userBookmarks.value?.find(b => b.bookmarkId === props.item.id));
-    const likedItem = computed(() => _userLikes.value?.find(b => b.bookmarkId === props.item.id));
-
-    const _getCurrentUser = computed(() => store.getters._getCurrentUser);
-    const _userLikes = computed(() => store.getters._userLikes);
-    const _userBookmarks = computed(() => store.getters._userBookmarks);
-</script>
